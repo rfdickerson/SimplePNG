337a3b3ae9ca93addcda2a66ef962c4e4cc6e2c6
Robert F. Dickerson <rfdickerson@us.ibm.com>
2017-01-02 18:15:22 -0600

    Pixel representation

--- Sources/SimplePNG.swift
+++ Sources/SimplePNG.swift
@@ -1,7 +1,35 @@
 import Foundation
 import CPNG
 
-typealias Pixel = UInt8
+public typealias Bitmap = [[Pixel]]
+
+public enum Pixel {
+    
+    case srgb(Float, Float, Float)
+    case rgb(UInt8, UInt8, UInt8)
+    case rgba(UInt8, UInt8, UInt8, UInt8)
+    case gray(Float)
+    
+    init(red: Float, green: Float, blue: Float) {
+        self = .srgb(red, green, blue)
+    }
+    
+}
+
+extension Pixel {
+    public var bytes: [UInt8] {
+        switch self {
+        case .srgb(let r, let g, let b):
+            return [UInt8(pow(r,2.2)*255), UInt8(pow(g,2.2)*255), UInt8(pow(b,2.2)*255)]
+        case .rgb(let r, let g, let b):
+            return [r, g, b]
+        case .rgba(let r, let g, let b, let a):
+            return [r,g,b,a]
+        case .gray(let v):
+            return [UInt8(v*255)]
+        }
+    }
+}
 
 enum SimpleImageError: Error {
     case writeError
@@ -25,32 +53,32 @@ public enum ColorType {
 
 public class Image {
     
-    var width: Int
-    var height: Int
-    var colorType: ColorType
-    var bitDepth: Int
+    public var width: Int
+    public var height: Int
+    public var colorType: ColorType
+    public var bitDepth: Int
     
-    var rows: [[Pixel]]?
+    var bitmap: Bitmap?
     
-    init(width: Int,
-         height: Int,
-         colorType: ColorType,
-         bitDepth: Int,
-         rows: [[Pixel]]? = nil) {
+    public init(width: Int,
+                height: Int,
+                colorType: ColorType,
+                bitDepth: Int,
+                bitmap: Bitmap? = nil) {
         
         self.width = width
         self.height = height
         self.colorType = colorType
         self.bitDepth = bitDepth
-        self.rows = rows
+        self.bitmap = bitmap
         
     }
 }
 
 extension Image {
-
-    func write(to url: URL) throws {
-     
+    
+    public func write(to url: URL) throws {
+        
         let fp = fopen(url.relativeString, "wb")
         
         let pngPtr = png_create_write_struct(PNG_LIBPNG_VER_STRING, nil, nil, nil);
@@ -73,18 +101,29 @@ extension Image {
                      PNG_INTERLACE_NONE,
                      PNG_COMPRESSION_TYPE_DEFAULT,
                      PNG_FILTER_TYPE_DEFAULT);
-
+        
         png_write_info(ptr, info_ptr);
         
-        guard let rows = rows else {
-            throw SimpleImageError.writeError
-        }
+        // var bitmapData = [[UInt8]]()
+        
+        // Convert pixels to byte-array
+        let bitmapData: [[UInt8]] = bitmap.flatMap { row in
+            
+            row.map { pixels in
+                
+                pixels.flatMap {
+                    $0.bytes
+                }
+                
+            }
+            
+            }!
         
         // write the bytes
-        for row in rows {
+        for row in bitmapData {
             png_write_row(ptr, row);
         }
-
+        
         
         png_write_end(ptr, nil);
         
